14c8d6149a66b98011e787761607f026bc3a016f
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-04-24 20:39:46 -0400

    saved progress

--- handlers/training.py
+++ handlers/training.py
@@ -1,32 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+import json
 import pandas as pd
 import joblib
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
+from sklearn.linear_model import LogisticRegression
 from preprocessor import csv
-
+import matplotlib.pyplot as plt
 router = APIRouter()
 
-@router.get("/train/{model_id}/{col}")
-async def train(model_id: str, col: str):
+@router.post("/train/{model_id}/{col}")
+async def train(model_id: str, col: str, request: Request):
+    body = await request.json()
+
     df = pd.read_csv(model_id + ".csv")
-    print(col)
     y = df[col]
-    X = df.drop(col, axis=1) 
+    X = df.drop(col, axis=1)
     variable_columns = list(X.columns)
 
     df, X, encoded_columns, columns, column_values = csv.process_and_encode(df, X)
-
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    # Create a linear regression model
+
+    type, subtype = body["type"], body["subtype"]
+
+    if (type == "linear-regression"):
+        if (subtype == "simple"):
+            pass
+        if (subtype == "polynomial"):
+            pass
+    
+         
+    
+def train_regression(df, X, y):
+    variable_columns = list(X.columns)
+
+    df, X, encoded_columns, columns, column_values = csv.process_and_encode(df, X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
     model = LinearRegression()
-
-    # Fit the model to the data
     model.fit(X_train, y_train)
-
 
     y_pred = model.predict(X_test)
 
@@ -43,6 +57,19 @@ async def train(model_id: str, col: str):
     # print("Learned Bias: {:.4f}".format(learned_b))
     joblib.dump(model, model_id + ".pkl")
 
+    plt.scatter(X_train, y_train)
+
+    # Plot the regression line
+    plt.plot(X_train, model.predict(X_train), color='red')
+
+    # Add labels and title
+    plt.xlabel('Independent Variable')
+    plt.ylabel('Dependent Variable')
+    plt.title('Linear Regression')
+
+    # Show the plot
+    plt.savefig(model_id + ".png")
+
     return {
         "orginal_columns": list(df.columns),
         "column_headers": variable_columns, 
@@ -51,3 +78,17 @@ async def train(model_id: str, col: str):
         "column_values": column_values,
         "target_column": ["charges"],
         }
+
+def train_logistic():
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=42)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    model = LogisticRegression(random_state=42)
+    model.fit(X_train, y_train)
+
+    score = model.score(X_test, y_test)
+    print("Accuracy:", score)
+    pass
